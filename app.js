0c1690bc158edc78d6e007d970fae7480a6ed37b
ben <ben.hankins@ibm.com>
2020-03-26 12:41:36 -0500

    Changed title to Cloud Paks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody Demo!");
+  res.send("Hello from Cloud Pak for Apps!!!!!");
 });
  
 module.exports.app = app;
